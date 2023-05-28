40438a92593298f1c31a5448fc179c5955f682bf
oussamaZAAM <zaam.oussama@gmail.com>
2023-05-28 02:24:41 +0100

    Add filtering and search querying

--- controllers/review.js
+++ controllers/review.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST, // Values in ".env" file
+  user: process.env.DB_USER, // Values in ".env" file
+  password: process.env.DB_PASSWORD, // Values in ".env" file
+  database: process.env.DB_DATABASE, // Values in ".env" file
+});
+
 // Get all reviews Controller
 const getAllReviews = async (req, res) => {
   const selectQuery = "SELECT * FROM reviews";
-
-  // Create a MySQL connection pool
-  const pool = mysql.createPool({
-    host: process.env.DB_HOST, // Values in ".env" file
-    user: process.env.DB_USER, // Values in ".env" file
-    password: process.env.DB_PASSWORD, // Values in ".env" file
-    database: process.env.DB_DATABASE, // Values in ".env" file
-  });
 
   pool.getConnection((err, connection) => {
     if (err) {
@@ -51,6 +51,87 @@ const getAllReviews = async (req, res) => {
   });
 };
 
+const filterReviews = (req, res) => {
+  // Get the query parameters from the request
+  const { appID, appStoreName, rating, countryName } = req.query;
+
+  console.log(appID)
+
+  // Perform filtering based on the provided criteria
+  // Assuming you have a "reviews" table in your database
+
+  let sql = "SELECT * FROM reviews WHERE 1=1";
+
+  if (appID) {
+    sql += ` AND appID = '${appID}'`;
+  }
+
+  if (appStoreName) {
+    sql += ` AND appStoreName = '${appStoreName}'`;
+  }
+
+  if (rating) {
+    sql += ` AND rating = ${rating}`;
+  }
+
+  if (countryName) {
+    sql += ` AND countryName = '${countryName}'`;
+  }
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting a connection from the pool:", err);
+      res.status(500).json({ error: "An error occurred" });
+      return;
+    }
+
+    connection.query(sql, (err, results) => {
+      connection.release(); // Release the connection back to the pool
+
+      if (err) {
+        console.error("Error executing the query:", err);
+        res.status(500).json({ error: "An error occurred" });
+        return;
+      }
+
+      res.json({ results });
+    });
+  });
+};
+
+const searchReviews = (req, res) => {
+  const { searchQuery } = req.query;
+
+  if (!searchQuery) {
+    res.status(400).json({ error: "Missing search query parameter" });
+    return;
+  }
+
+  const sql = `SELECT * FROM reviews WHERE reviewHeading LIKE '%${searchQuery}%' OR reviewText LIKE '%${searchQuery}%'`;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting a connection from the pool:", err);
+      res.status(500).json({ error: "An error occurred" });
+      return;
+    }
+
+    connection.query(sql, (err, results) => {
+      connection.release(); // Release the connection back to the pool
+
+      if (err) {
+        console.error("Error executing the query:", err);
+        res.status(500).json({ error: "An error occurred" });
+        return;
+      }
+
+      res.json({ results });
+    });
+  });
+};
+
 module.exports = {
   getAllReviews,
+  filterReviews,
+  searchReviews
 };
--- routes/review.js
+++ routes/review.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getAllReviews } = require("../controllers/review");
+const { getAllReviews, filterReviews, searchReviews } = require("../controllers/review");
 
 const ReviewRouter = express.Router();
 
 
 ReviewRouter.get('/', getAllReviews);
+ReviewRouter.get('/filter', filterReviews);
+ReviewRouter.get('/search', searchReviews);
 
 
 module.exports = {ReviewRouter};
